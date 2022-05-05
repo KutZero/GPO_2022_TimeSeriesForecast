658c30fe2deb0ff7fada98e487ec76ba3833a008
KutZero <82716275+KutZero@users.noreply.github.com>
2022-05-05 18:03:09 +0700

    Add docs to modules

--- custom_modules/Data_processing.py
+++ custom_modules/Data_processing.py
@@ -1,32 +1,39 @@
+""" Модуль для всяческой обработки данных перед обучением модели.
+
+Функции:
+
+get_df([path]) - Чтение датасета из Excel;
+
+delete_outliers([df], [soft_mode = True]) - Удалить выбросы с помощью IQR;
+
+standartize_data([df]) - Стандартизовать данные;
+
+normalize_data([df]) - Нормализовать данные;
+
+delete_NANs([df]) - Удалить строки с пустыми значениями;
+
+delete_zeros([df]) - Удалить строки с нулевыми значениями.
+
+Более подробнаую информацию можно получить так:
+
+1) В Jupyter Notebook: "?[Имя модуля].[Имя функции]";
+2) В общем виде: "print('[Имя модуля].[Имя функции].__doc__')";
+3) В общем виде: "help([Имя модуля].[Имя функции])".
+"""
+
 import pandas as pd
-#import numpy as np
-
-#описание модуля
-def module_description():
-    print('Функции:\n')
-
-    print('Чтение датасета из Excel - get_df([path])')
-    print('[path] - строка, путь к файлу\n')
-
-    print('Удалить выбросы с помощью IQR - delete_outliers([df], [soft_mode = True])')
-    print('[df] - датасет для обработки')
-    print('[soft_mode = True] - режим, True - мягкое удаление выбросов',
-    '\n если False - грубое удаление выбросов\n')
-
-    print('Стандартизовать данные - standartize_data(df)')
-    print('[df] - датасет для обработки\n')
-
-    print('Нормализовать данные - normalize_data(df)')
-    print('[df] - датасет для обработки\n')
-
-    print('Удалить строки с пустыми значениями - delete_NANs(df)')
-    print('[df] - датасет для обработки\n')
-
-    print('Удалить строки с нулевыми значениями - delete_zeros(df)')
-    print('[df] - датасет для обработки\n')
+import numpy as np
 
 # чтение данных из файла Excel и возврат датафрейма
 def get_df(path):
+    """Возвращает датафрейм, считанный из Excel файла.
+
+    :param path: arg1
+    :type path: string
+
+    :rtype: DataFrame
+    :return: Датафрейм с данными из файла
+    """
     file = pd.read_excel(path)
     file.rename(columns = {'Unnamed: 0' : 'Time Moment'}, inplace = True)
     df = file.drop(labels = [0], axis = 0) # удалить вторую строку оглавления
@@ -34,11 +41,22 @@ def get_df(path):
     columns = list(df.columns[1:])
     for col in columns: #настроить типы данных столбцов
         df[col] = df[col].astype(float)
+
     return df
 
 # удалить выбросы из данных методом IQR
 # soft_mode - рамки удаления выбросов, если True - удаляет только самые вопиющие выбросы
 def delete_outliers(df, soft_mode = True):
+    """Возвращает датафрейм, очищенный от выбросов IQR методом.
+
+    :param df: arg1
+    :type df: DataFrame
+    :param soft_mode: arg2, defaults to True
+    :type soft_mode: Boolean, optional
+
+    :rtype: DataFrame
+    :return: Датафрейм без выбросов
+    """
     columns = list(df.columns[1:])
     dataset_size = df.shape
 
@@ -56,11 +74,21 @@ def delete_outliers(df, soft_mode = True):
         df = df[(df[col] > (Q1-1.5*IQR)) & (df[col] < (Q3+1.5*IQR))]
         print('\tУдалено строк: ', row_before - df.shape[0], "\n")
 
-    print("Размер датасета до обработки: ", dataset_size, " .После: ", df.shape)
+    print("Размер датасета:\nдо обработки: ", dataset_size, "\nпосле: ", df.shape,
+        "\nудалено строк: ", (dataset_size[0]-df.shape[0]))
+
     return df
 
 # стандартизация данных (от -1 до 1)
 def standartize_data(df):
+    """Возвращает стандартизированный датафрейм.
+
+    :param df: arg1
+    :type df: DataFrame
+
+    :rtype: DataFrame
+    :return: Стандартизированный датафрейм
+    """
     columns = list(df.columns[1:])
 
     for col in columns:
@@ -70,6 +98,14 @@ def standartize_data(df):
 
 # нормализация данных (от 0 до 1)
 def normalize_data(df):
+    """Возвращает нормализованный датафрейм.
+
+    :param df: arg1
+    :type df: DataFrame
+
+    :rtype: DataFrame
+    :return: Нормализованный датафрейм
+    """
     columns = list(df.columns[1:])
 
     for col in columns:
@@ -79,17 +115,38 @@ def normalize_data(df):
 
 # удалить строки с пустыми значениями
 def delete_NANs(df):
+    """Возвращает датафрейм, очищенный от NAN значений.
+
+    :param df: arg1
+    :type df: DataFrame
+
+    :rtype: DataFrame
+    :return: Датафрейм без NAN значений
+    """
     dataset_size = df.shape
     df.dropna(inplace = True)
-    print("Размер датасета до обработки: ", dataset_size, " .После: ", df.shape)
+    print("Размер датасета:\nдо обработки: ", dataset_size, "\nпосле: ", df.shape,
+        "\nудалено строк: ", (dataset_size[0]-df.shape[0]))
+
     return df
 
 # удалить строки с нулевыми значениями
 def delete_zeros(df):
+    """Возвращает датафрейм, очищенный от нулевых значений.
+
+    :param df: arg1
+    :type df: DataFrame
+
+    :rtype: DataFrame
+    :return: Датафрейм без нулевых значений
+    """
     dataset_size = df.shape
     columns = list(df.columns[1:])
 
     for col in columns:
         df = df.loc[df[col] != 0]
-    print("Размер датасета до обработки: ", dataset_size, " .После: ", df.shape)
+
+    print("Размер датасета:\nдо обработки: ", dataset_size, "\nпосле: ", df.shape,
+        "\nудалено строк: ", (dataset_size[0]-df.shape[0]))
+
     return df
--- custom_modules/test.py
+++
@@ -1,4 +0,0 @@
-import Data_processing as dp
-#import Data_visualization as dv
-
-dp.get_df('Data\Данные СТМ Восточный Купол (от 29.01.17)\Куст 202\Скважина 2022.xlsx')
